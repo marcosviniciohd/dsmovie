6bfe971cdb0b6c07313da24933f67b3b7bdfb8be
Marcos <marcosviniciohd@hotmail.com>
2022-01-12 22:56:14 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar(){
+    return(
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github/marcosviniciohd">
+                        <div className='dsmovie-contact-container'>
+                            <GithubIcon/>
+                            <p className='dsmovie-contact-link'>/marcosviniciohd</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
