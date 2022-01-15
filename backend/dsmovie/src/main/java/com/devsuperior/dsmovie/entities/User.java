5ad343d65bc901243657bb1c03ef46a7a9eac794
Marcos <marcosviniciohd@hotmail.com>
2022-01-15 19:44:40 -0300

    Domain model, database seed

---
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	
+	
+	public User() {
+		
+	}
+
+
+	public User(Long id, String nome) {
+		this.id = id;
+		this.email = nome;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return email;
+	}
+
+
+	public void setNome(String nome) {
+		this.email = nome;
+	}
+
+	
+}
