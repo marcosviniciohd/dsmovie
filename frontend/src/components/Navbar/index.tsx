debbb1efd8758c2666b13c570dd04e024196cec4
Marcos <marcosviniciohd@hotmail.com>
2022-01-17 18:00:18 -0300

    Props

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar(){
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
                     <h1>DSMovie</h1>
-                    <a href="https://github/marcosviniciohd">
+                    <a href="https://github.com/marcosviniciohd" target="_blank">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon/>
                             <p className='dsmovie-contact-link'>/marcosviniciohd</p>
